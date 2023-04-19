3c43964156c51fed0d76a9221adab54ac8f2d600
Manuel Santiago Cabeza <msantiagoc33@gmail.com>
2023-04-19 12:07:06 +0200

    Primer commit

---
+++ scripts/script1_santiago.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Clase encargada de realizar la conexión con la BBDD.
+ * 
+ * @author Manuel Santiago Cabeza
+ * @version 1.0
+ * Hace uso de {@link Conexion/ConexionPDO.php#conectar()}
+ * @throws Exception lanzada cuando no pueda realizarse la conexión.
+ */
+class Login {
+
+    public function __construct() {
+        try {
+            $this->conexion = conexion::conectar();
+        } catch (Exception $ex) {
+            die($ex->getMessage());
+        }
+    }
+}
---
+++ scripts/script2_santiago.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Clase que se conecta a la BBDD y selecciona clientes por paises.
+ * @author Manuel Santiago Cabeza
+ * @version 1.1 
+ */
+class Clientes {
+
+    public function __construct() {
+        $this->conexion = Conexion::conectar();
+    }
+
+    /**
+     * Función encargada de seleccionar clientes y paises de la BBDD.
+     * Se seleccionarán los clientes cuyo id de pais de la tabla pais 
+     * coincida con el id de pais de la tabla clientes
+     * @version 1.2
+     * @return devuelve el conjunto de registros que cumple con las condiciones dadas.
+     * Es llamada por {@link controlador#todosLosClientes()}
+     * @param pais, parámetro recibido para buscar por el pais pasado por el mismo
+     */
+    public function todosLosClientes(String $pais) {
+        $sqlClientes = "SELECT
+    c.id_cliente,
+    c.nombre as cliente,
+    p.nombre as pais
+FROM
+    clientes AS c,
+    paises AS p    
+WHERE
+    c.id_pais = $pais
+ORDER BY c.id_cliente DESC";
+        $resultado = $this->conexion->prepare($sqlClientes);
+        $resultado->execute();
+        return $resultado->fetchAll(PDO::FETCH_OBJ);
+    }
+}
